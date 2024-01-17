f1d53bdd4c2638bd09d65fec75114dd6e1d2d6c0
satcom <raspsatcombot@gmail.com>
2024-01-17 14:45:06 +0000

    Fix bug in login functionality

---
+++ transpose/src/main.rs
@@ -0,0 +1,23 @@
+fn transpose(matrix: &mut [[i32; 3]; 3]) {
+    for i in 0..3 {
+        for j in i + 1..3 {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+fn main() {
+    let mut matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+
+    transpose(&mut matrix);
+
+    for row in matrix.iter() {
+        println!("{:?}", row);
+    }
+}
